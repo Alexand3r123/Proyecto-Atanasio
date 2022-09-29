f14843310dea29d5be95bbbf4c2649720a32ca7b
Merostyle <fgrisales.4321@gmail.com>
2022-09-29 15:51:03 -0500

    Carosuel

---
+++ js/main.js
@@ -0,0 +1,39 @@
+var imagenes =  ['images/main/img1.jpg',  'images/main/img2.jpg', 'images/main/img3.jpg'],
+    cont = 0;
+
+    function carrusel(contenedor) {
+        contenedor.addEventListener('click', e => {
+            let atras = contenedor.querySelector('.atras'),
+                adelante = contenedor.querySelector ('.adelante'),
+                img = contenedor.querySelector('img'),
+                tgt = e.target;
+
+
+                if (tgt == atras){
+                    if(cont > 0) {
+                        img.src = imagenes [cont - 1];
+                        cont --;
+                    } else {
+                        img.src = imagenes [imagenes.length - 1];
+                        cont = imagenes.length - 1;
+                    }
+
+                } else if (tgt == adelante){
+                    if(cont < imagenes.length - 1) {
+                        img.src = imagenes [cont + 1];
+                        cont ++;
+                    } else {
+                        img.src = imagenes [0];
+                        cont = 0;
+                    }
+
+                }
+        });
+    }
+    document.addEventListener("DOMContentLoaded", () =>{
+        let contenedor = document.querySelector('.contenedor');
+
+        carrusel(contenedor);
+    });
+
+    

6146aa241ca9fc2ce9c4dd67100578d9d8a55f6f
AlexMarin099 <alexmarinci576qgmail.com>
2023-02-15 11:45:59 -0500

    proyecto

---
+++ Res-Proyecto/Estudiantes/Recuperacion.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <link href="https://fonts.googleapis.com/css2?family=Akaya+Kanadaka&display=swap" rel="stylesheet">
+    <title>Recuperar contaseña</title>
+    </head>
+<body>
+	<section>
+		 <div class="container">
+			 	<div class="form">
+			 		<h2>Recuperar contraseña</h2>
+			 		<form >
+			 			<div class="inputBox">
+			 				<input type="text" placeholder="Correo electronico"/>
+			 			</div>
+			 			<div class="inputBox">
+			 				<input type="submit" value="Enviar" />
+			 			</div>
+			 			<p class="forget"><a href="index.php">Iniciar sesión</a>
+			 			</p>
+			 		</form>
+			 	</div>
+			 </div>
+		</div>
+	</section>
+</body>
+</html>
